f1d095c2d99b3966541cd921d246fcca4332fc8a
Laziz200 <lazizshakarov243@gmail.com>
2025-04-24 20:12:38 +0500

    .

---
+++ homework/7-masala/index.js
@@ -0,0 +1,13 @@
+var son=Number(prompt("arrayda nechta son saqlamoqchisiz?"));
+var arr=[];
+for (let i = 1; i <= son; i++) {
+    var num=Number(prompt(`${i} chi sonni kiriting:`));
+    arr[arr.length]=num;
+    
+}
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i]%2==1) {
+         delete arr[i];
+    }
+}
+console.log(arr);
